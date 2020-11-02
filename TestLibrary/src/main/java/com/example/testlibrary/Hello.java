4d05de84fb183c260bb2c2d7524c7975fd1c0702
Teja <salanatejesh@gmail.com>
2020-11-02 22:18:20 +0530

    created Hello class in Test library

---
+++ TestLibrary/src/main/java/com/example/testlibrary/Hello.java
@@ -0,0 +1,12 @@
+package com.example.testlibrary;
+
+import android.util.Log;
+
+public class Hello {
+    private static final String TAG = "Hello";
+
+    public static void greet(){
+        Log.d(TAG, "greet: hello, good morning user.....");
+    }
+
+}
