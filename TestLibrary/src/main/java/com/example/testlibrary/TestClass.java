f36512a7415630efe5546a71a5ed6083807e60fc
Teja <salanatejesh@gmail.com>
2020-11-16 08:36:03 +0530

    added Test class

---
+++ TestLibrary/src/main/java/com/example/testlibrary/TestClass.java
@@ -0,0 +1,11 @@
+package com.example.testlibrary;
+
+import android.util.Log;
+
+public class TestClass {
+    private static final String TAG = "TestClass";
+    
+    public void print(){
+        Log.d(TAG, "print: Test printing....");
+    }
+}
